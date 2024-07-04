1f973441e8f4eabb782f2d797239cc804d524c1e
clydeqin7 <clydeqin7@foxmail.com>
2024-07-04 14:46:27 +0800

    init

---
+++ src/main/java/com/fe4fun/App.java
@@ -0,0 +1,13 @@
+package com.fe4fun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
