85e01e9cca6baafd3450a0e4949a68fddacf555d
clydeqin7 <clydeqin7@foxmail.com>
2024-07-11 10:03:22 +0800

    feat:登录密码加密

--- src/main/java/com/fe4fun/service/UserService.java
+++ src/main/java/com/fe4fun/service/UserService.java
@@ -1,19 +1,47 @@
 package com.fe4fun.service;
 
 import com.fe4fun.entity.User;
-import com.fe4fun.mapper.UserMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class UserService {
-    private UserMapper userMapper;
+@Service
+public class UserService implements UserDetailsService {
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Map<String, String> userPasswords = new ConcurrentHashMap<>();
 
     @Inject
-    public UserService(UserMapper userMapper) {
-        this.userMapper = userMapper;
+    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        save("gebilaowang", "gebilaowang");
+    }
+
+    public void save(String username, String password) {
+        userPasswords.put(username, bCryptPasswordEncoder.encode(password));
+    }
+
+    public String getPassword(String username) {
+        return userPasswords.get(username);
     }
 
     public User getUserById(Integer id) {
-        return userMapper.findUserById(id);
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (!userPasswords.containsKey(username)) {
+            throw new UsernameNotFoundException(username + "不存在");
+        }
+        String encodedPassword = userPasswords.get(username);
+
+        return new org.springframework.security.core.userdetails.User(username, encodedPassword, Collections.emptyList());
     }
 }
